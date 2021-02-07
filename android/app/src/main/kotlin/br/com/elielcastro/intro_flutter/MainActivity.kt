5c18d882158449a25352a333459bb19789d05b98
Eliel Castro <mail@elielcastro.com.br>
2021-02-06 21:52:16 -0300

    code upload

---
+++ android/app/src/main/kotlin/br/com/elielcastro/intro_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.elielcastro.intro_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new Center(
+      child: new Text("Olá, João!",
+        textDirection: TextDirection.ltr
+      ),
+    )
+  );
+}
